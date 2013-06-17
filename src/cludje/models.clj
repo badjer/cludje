103b41d3fb169a7a000085cfc2d0acedeaf00533
badjer <brian.dejong@gmail.com>
2013-06-17 09:51:17 -0700

    Removed models

--- src/cludje/models.clj
+++
@@ -1,8 +0,0 @@
-(ns cludje.models
-  (:use cludje.types
-        cludje.core))
-
-(defmodel User {:email Email :password Password})
-
-;(defmodel User 
-  ;{:fields {:email Email :password Password}})
