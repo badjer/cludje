83a38d549181cdf26c893237e9b62a96744c2d50
badjer <brian.dejong@gmail.com>
2013-07-03 11:30:41 -0700

    Angularjs share model data across templates

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -2,4 +2,12 @@ angular.module('mainapp', [], function($routeProvider, $locationProvider){
 	$routeProvider.when('/hello', {
 		templateUrl: 'hello.tpl.html'
 	});
+	$routeProvider.when('/goodbye', {
+		templateUrl: 'goodbye.tpl.html'
+	});
 });
+
+function MainCntl($scope){
+	$scope.data = {}
+};
+
