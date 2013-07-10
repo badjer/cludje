52f9f8003b2c1cd6ac27b1ad0fd5d5dd17c2f635
badjer <brian.dejong@gmail.com>
2013-07-10 15:47:20 -0700

    inc'd version #

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cludje "0.0.7"
+(defproject cludje "0.0.8"
   :description "Cludje - A clojure web framework"
   :url "http://github.com/badjer/cludje"
   :license {:name "Eclipse Public License"
