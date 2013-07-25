fc8e02b385e289ec44dfe8e4c0ea0711aa4b1246
badjer <brian.dejong@gmail.com>
2013-07-25 09:39:42 -0700

    Actions can be specified with keywords (nice for testing)

--- src/cludje/actionparser.clj
+++ src/cludje/actionparser.clj
@@ -5,7 +5,8 @@
 (defrecord ActionParser []
   IActionParser
   (get-action-name- [self input]
-    (get input :_action))
+    (when-let [action-name (get input :_action)]
+      (name action-name)))
   (get-model-name- [self input]
     (when-let [action (get-action-name- self input)]
       (when-let [s (second (reverse (s/split action #"-")))]
--- test/cludje/actionparser_test.clj
+++ test/cludje/actionparser_test.clj
@@ -6,10 +6,12 @@
 (let [ap (->ActionParser)]
   (facts "get-model-name"
     (get-model-name- ap {:_action "cog-add"}) => "Cog"
+    (get-model-name- ap {:_action :cog-add}) => "Cog"
     (get-model-name- ap {}) => nil
     (get-model-name- ap nil) => nil)
   (facts  "get-action-key-"
     (get-action-key- ap {:_action "cog-add"}) => :add
+    (get-action-key- ap {:_action :cog-add}) => :add
     (get-action-key- ap {}) => nil
     (get-action-key- ap nil) => nil)
   (facts "works with initial - (ie, for -system model)"
