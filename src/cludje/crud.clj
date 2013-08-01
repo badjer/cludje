39d4f0d1ce82c7582059f3bcdafdc8e9435ead34
badjer <brian.dejong@gmail.com>
2013-07-31 20:14:31 -0700

    Cleanup crud

--- src/cludje/crud.clj
+++ src/cludje/crud.clj
@@ -40,10 +40,11 @@
 (defn realize-map [system input m]
   (into {} (for [[k v] m] [k (realize-one system input v)])))
 
-(defn make-crud-model-list [model]
-  `(let [listkee# (keyword (str (table-name ~model) "s"))
-         parts# (partitions ~model)
-         defs# (defaults ~model)
+
+(defn crud-model-list [model system input]
+  (let [listkee (keyword (str (table-name model) "s"))
+         parts (partitions model)
+         defs (defaults model)
          ; This is a bit complicated because of optimization
          ; We want parts-def to be (select-keys (realize-map defs)),
          ; but we do an extra internal select-keys inside of 
@@ -52,47 +53,44 @@
          ; The reason we add a (make model..) onto this is because
          ; we want to handle the case where the default is of the 
          ; wrong type - this will convert it
-         part-defs# (select-keys 
-                      (make ~model
-                            (realize-map ~'system ~'input
-                                         (select-keys defs# parts#)))
-                      parts#)
+         part-defs (select-keys 
+                      (make model
+                            (realize-map system input
+                                         (select-keys defs parts)))
+                      parts)
          ; We want to get a parsed version of any of the partitions
          ; that the user passed as input - that's part-inp
-         parsed# (make ~model ~'input)
-         supplied-parts# (clojure.set/intersection 
-                           (set parts#) 
-                           (set (keys ~'input)))
-         part-inp# (select-keys parsed# supplied-parts#)
+         parsed (make model input)
+         supplied-parts (clojure.set/intersection 
+                           (set parts) 
+                           (set (keys input)))
+         part-inp (select-keys parsed supplied-parts)
          ; The query-paras are the defaults and input of the partitions
-         query-paras# (merge part-defs# part-inp#)]
-     {listkee# (~'query ~model query-paras#)}))
+         query-paras (merge part-defs part-inp)]
+     {listkee (query system model query-paras)}))
 
-(defn make-crud-model-new [model]
-  `(realize-map ~'system ~'input (defaults ~model)))
+(defn crud-model-new [model system input]
+  (realize-map system input (defaults model)))
 
-(defn make-crud-model-add [model]
-  `(let [defs# (make ~model (realize-map ~'system ~'input (defaults ~model)))
-         combined# (merge defs# ~'input)] 
-     (-> (~'insert ~model combined#) 
-         (with-alert :success "Saved"))))
+(defn crud-model-add [model system input]
+  (let [defs (make model (realize-map system input (defaults model)))
+        combined (merge defs input)]
+    (-> (insert system model combined)
+        (with-alert :success "Saved"))))
 
-(defn make-crud-model-show [model]
-  `(~'fetch ~model (~'? (key-name ~model))))
+(defn crud-model-show [model system input]
+  (fetch system model (? input (key-name model))))
 
-(defn make-crud-model-edit [model]
-  `(~'fetch ~model (~'? (key-name ~model))))
+(defn crud-model-edit [model system input]
+  (fetch system model (? input (key-name model))))
 
-(defn make-crud-model-alter [model]
-  `(do 
-     (~'? (key-name ~model))
-     (-> (~'save ~model ~'input)
-         (with-alert :success "Saved"))))
+(defn crud-model-alter [model system input]
+  (? input (key-name model))
+  (-> (save system model input)
+      (with-alert :success "Saved")))
 
-(defn make-crud-model-delete [model]
-  `(do
-     (~'delete ~model (~'? (key-name ~model)))
-     nil))
+(defn crud-model-delete [model system input]
+  (delete system model (? input (key-name model))))
 
 
 (defmacro def-crud-actions [model-sym]
@@ -100,19 +98,19 @@
         modelname (table-name model)]
     `(do
        (defaction ~(symbol (str modelname "-list"))
-         ~(make-crud-model-list model-sym))
+         (crud-model-list ~model-sym ~'system ~'input))
        (defaction ~(symbol (str modelname "-new"))
-         ~(make-crud-model-new model-sym))
+         (crud-model-new ~model-sym ~'system ~'input))
        (defaction ~(symbol (str modelname "-add"))
-         ~(make-crud-model-add model-sym))
+         (crud-model-add ~model-sym ~'system ~'input))
        (defaction ~(symbol (str modelname "-show"))
-         ~(make-crud-model-show model-sym))
+         (crud-model-show ~model-sym ~'system ~'input))
        (defaction ~(symbol (str modelname "-edit"))
-         ~(make-crud-model-edit model-sym))
+         (crud-model-edit ~model-sym ~'system ~'input))
        (defaction ~(symbol (str modelname "-alter"))
-         ~(make-crud-model-alter model-sym))
+         (crud-model-alter ~model-sym ~'system ~'input))
        (defaction ~(symbol (str modelname "-delete"))
-         ~(make-crud-model-delete model-sym)))))
+         (crud-model-delete ~model-sym ~'system ~'input)))))
 
 (defn with-lookup- [m model system input]
   (let [action-name (str (table-name model) "-list")
