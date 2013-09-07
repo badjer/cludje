4220547e607b23cfa531d675019ba6890dc04aaf
badjer <brian.dejong@gmail.com>
2013-09-07 15:10:53 -0700

    Removed extraneous commented-out code from serve_test

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cludje "0.2.8"
+(defproject cludje "0.2.9"
   :description "Cludje - A clojure web framework"
   :url "http://github.com/badjer/cludje"
   :license {:name "Eclipse Public License"
